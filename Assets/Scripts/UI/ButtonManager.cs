b36755f47011e60bf75b57fd611c02755560642e
rafaelhaertel <rhperes@inf.ufpel.edu.br>
2018-04-16 14:43:45 -0300

    Tech demo
    Created a tech demo for week 2

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void CarregarFase1(){
+		SceneManager.LoadScene ("FirstStage");
+	}
+}
